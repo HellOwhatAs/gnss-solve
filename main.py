2f5fd37fd2703c2adea2b0c5c8cd825adb7221d9
HellOwhatAs <xjq701229@outlook.com>
2025-07-01 23:07:23 +0800

    add plot

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import gnss_lib_py as glp
 from utils import get_fde_estimate, estimate_distance, distance_category
 from skymask_example import get_skymask_estimate
 from weighted_example import get_weighted_estimate
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import matplotlib
 
 
 def main():
@@ -18,12 +22,35 @@ def main():
 
         glp.plot_map(
             fde_estimate, skymask_estimate, weighted_estimate, fix_data
-        ).write_html(f"output/skymask_estimate_map_{i}.html")
+        ).write_html(f"output/estimate_map_{i}.html")
 
         skymask_df = estimate_distance(skymask_estimate, fix_data).pandas_df()
         weighted_df = estimate_distance(weighted_estimate, fix_data).pandas_df()
         skymask_df["category"] = skymask_df["distance_m"].apply(distance_category)
         weighted_df["category"] = weighted_df["distance_m"].apply(distance_category)
+
+        for name, df in {"skymask": skymask_df, "weighted": weighted_df}.items():
+            li = df["category"].tolist()
+            print(i, name, {c: li.count(c) for c in "ABCD"}, "total", len(li))
+
+        matplotlib.rcdefaults()
+        sns.displot(
+            data=pd.DataFrame(
+                {
+                    "skymask_distance_m": skymask_df["distance_m"],
+                    "weighted_distance_m": weighted_df["distance_m"],
+                }
+            ).melt(
+                value_vars=["skymask_distance_m", "weighted_distance_m"],
+                var_name="method",
+                value_name="distance_m",
+            ),
+            x="distance_m",
+            hue="method",
+            kind="kde",
+        )
+        plt.savefig(f"output/estimate_plot_{i}.pdf")
+
         skymask_df.to_excel(f"output/skymask_estimate_{i}.xlsx")
         weighted_df.to_excel(f"output/weighted_estimate_{i}.xlsx")
 
