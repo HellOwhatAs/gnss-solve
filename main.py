adcca290e63aa2412be1f13c6b1cd5b0248d26dd
HellOwhatAs <xjq701229@outlook.com>
2025-07-01 22:24:45 +0800

    output excel

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+import os
 import gnss_lib_py as glp
-from utils import get_fde_estimate
+from utils import get_fde_estimate, estimate_distance, distance_category
 from skymask_example import get_skymask_estimate
 from weighted_example import get_weighted_estimate
 
 
 def main():
+    if not os.path.exists("output"):
+        os.makedirs("output")
+
     gnss_log_file_paths = [f".local/gnss_log_{i}.csv" for i in range(1, 5)]
     for i, gnss_log_file_path in enumerate(gnss_log_file_paths, start=1):
         fix_data = glp.AndroidRawFixes(gnss_log_file_path)
         fde_estimate = get_fde_estimate(gnss_log_file_path)
         skymask_estimate = get_skymask_estimate(gnss_log_file_path)
         weighted_estimate = get_weighted_estimate(gnss_log_file_path)
+
         glp.plot_map(
             fde_estimate, skymask_estimate, weighted_estimate, fix_data
-        ).write_html(f"skymask_estimate_map_{i}.html")
+        ).write_html(f"output/skymask_estimate_map_{i}.html")
+
+        skymask_df = estimate_distance(skymask_estimate, fix_data).pandas_df()
+        weighted_df = estimate_distance(weighted_estimate, fix_data).pandas_df()
+        skymask_df["category"] = skymask_df["distance_m"].apply(distance_category)
+        weighted_df["category"] = weighted_df["distance_m"].apply(distance_category)
+        skymask_df.to_excel(f"output/skymask_estimate_{i}.xlsx")
+        weighted_df.to_excel(f"output/weighted_estimate_{i}.xlsx")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 import gnss_lib_py as glp
 import cachier
+from gnss_lib_py.navdata.operations import find_wildcard_indexes
+from pygc import great_distance
+import numpy as np
+
+
+def distance_category(x: float) -> str:
+    return "A" if x < 10 else ("B" if x < 20 else ("C" if x < 50 else "D"))
 
 
 def name_mapper(new: str, old: str = "rx_wls"):
@@ -38,3 +45,35 @@ def get_fde_states(gnss_log_file_path: str) -> glp.NavData:
 @cachier.cachier()
 def get_fde_estimate(gnss_log_file_path: str) -> glp.NavData:
     return glp.solve_wls(get_fde_states(gnss_log_file_path))
+
+
+def estimate_distance(estimate1: glp.NavData, estimate2: glp.NavData):
+    e1_idxs, e2_idxs = (
+        find_wildcard_indexes(est, wildcards=["lat_*_deg", "lon_*_deg"])
+        for est in (estimate1, estimate2)
+    )
+    distance: list[float] = []
+    e2_lats_deg: list[float] = []
+    e2_lons_deg: list[float] = []
+    for timestamp, _, subset in glp.loop_time(estimate1, "gps_millis"):
+        pos1 = subset[e1_idxs["lat_*_deg"][0], e1_idxs["lon_*_deg"][0]]
+        pos2 = estimate2[
+            [e2_idxs["lat_*_deg"][0], e2_idxs["lon_*_deg"][0]],
+            np.argmin(np.abs(estimate2["gps_millis"] - timestamp)),
+        ]
+
+        e2_lats_deg.append(pos2[0])
+        e2_lons_deg.append(pos2[1])
+        distance.append(
+            great_distance(
+                start_latitude=pos1[0],
+                start_longitude=pos1[1],
+                end_latitude=pos2[0],
+                end_longitude=pos2[1],
+            )["distance"][0]
+        )
+
+    estimate1["distance_m"] = distance
+    estimate1[e2_idxs["lat_*_deg"][0]] = e2_lats_deg
+    estimate1[e2_idxs["lon_*_deg"][0]] = e2_lons_deg
+    return estimate1
